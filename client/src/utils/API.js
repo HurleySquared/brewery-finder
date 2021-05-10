d143e32bd7402866be93a6be3badd772c2a23106
Eric Hurley <erichurley421@gmail.com>
2021-05-10 13:22:16 -0400

    took out per_page in api fetch, was pulling random breweries instead of city search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   search: function (query) {
     return axios.get(
-      'https://api.openbrewerydb.org/breweries?per_page=25?by_city=' + query
+      'https://api.openbrewerydb.org/breweries?by_city=' + query
     )
   }
 };
