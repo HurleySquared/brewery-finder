4c997d85eb5c15a97c7db662b1b708f3c11c6aef
Eric Hurley <erichurley421@gmail.com>
2021-05-12 17:42:29 -0400

    added address into display with breweries

--- client/src/components/SearchResults/index.jsx
+++ client/src/components/SearchResults/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BrewContext from "../../utils/BrewContext";
+import "./results.css"
 
 function searchResults(props) {
 
@@ -16,17 +17,17 @@ function searchResults(props) {
     <BrewContext.Consumer>
       {({ result }) => (
       <>
-        <h2>Breweries in {props.heading}</h2>
+        <h3>Breweries in {props.heading}</h3>
         <ul className="brew-container">
           {result.map(brewski => {
             return (
               <li key={brewski.id} className="brewsList">
-                <p>{brewski.name} : <a 
-                    href="tel:555-555-5555"
-                    >
-                      {formatPhoneNumber(brewski.phone)}
+                <div className="brewHead">
+                  <h4>{brewski.name}</h4>
+                    <a href="tel:555-555-5555">
+                        {formatPhoneNumber(brewski.phone)}
                     </a>
-                </p>
+                </div>
                 <a 
                   href={brewski.website_url} 
                   target="_blank nonreferrer"
@@ -34,6 +35,9 @@ function searchResults(props) {
                 >
                   {brewski.website_url}
                 </a>
+                <address className="address">
+                  {brewski.street} {brewski.city}, {brewski.state} {brewski.postal_code}
+                </address>
             </li>
             )
           })}
